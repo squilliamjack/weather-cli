d51f230ae86a1222963932230a5f5844c63503f4
williamthesenate <williamp.jackson@calbaptist.edu>
2026-03-18 20:58:53 -0700

    Add error handling for invalid cities and network errors and while loop to retry on invalid input

--- weather.py
+++ weather.py
@@ -1,24 +1,36 @@
 import requests
 url = "https://api.openweathermap.org/data/2.5/weather"
 
-city = input("please input a city: ")
+while True:
+    city = input("please input a city: ")
 
-params = {
-    "q" : city,
-    "appid" : "cc5bd20949827335b8ae8b38710f0881",
-    "units" : "imperial"
-}
+    params = {
+        "q" : city,
+        "appid" : "cc5bd20949827335b8ae8b38710f0881",
+        "units" : "imperial"
+    }
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
 
-response = requests.get(url, params=params)
-data = response.json()
-temp = data["main"]["temp"]
-description = data["weather"][0]["description"]
-humidity = data["main"]["humidity"]
+        if data["cod"] != 200:
+            print(f"'{city}' not found. please check the spelling.")
+            print("-------------------------------------------------------")
+            
+        else: 
+            temp = data["main"]["temp"]
+            description = data["weather"][0]["description"]
+            humidity = data["main"]["humidity"]
 
-print("---------------------------")
-print(f"  Weather for {city.title()}")
-print("---------------------------")
-print(f"  Temperature : {temp}°F")
-print(f"  Conditions  : {description.title()}")
-print(f"  Humidity    : {humidity}%")
-print("---------------------------")
+            print("---------------------------")
+            print(f"  Weather for {city.title()}")
+            print("---------------------------")
+            print(f"  Temperature : {temp}°F")
+            print(f"  Conditions  : {description.title()}")
+            print(f"  Humidity    : {humidity}%")
+            print("---------------------------")
+            break
+
+    except requests.exceptions.RequestsException:
+        print("network error. check your connection and try again.")
+        print("----------------------------------------------------")
